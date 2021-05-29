9e86fbe212e5ec51da8d3a58839cf200ee578785
mihajlomelnik <melnik8686@gmail.com>
2021-05-29 23:50:57 +0300

    rest api @PostMapping @PutMapping @DeleteMapping

--- src/main/java/com/melnik/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/melnik/spring/rest/controller/MyRESTController.java
@@ -1,12 +1,9 @@
 package com.melnik.spring.rest.controller;
 
 import com.melnik.spring.rest.entity.Employee;
-import com.melnik.spring.rest.exeption_handling.EmployeeIncorrectData;
 import com.melnik.spring.rest.exeption_handling.NoSuchEmployeeException;
 import com.melnik.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,5 +28,27 @@ public class MyRESTController {
             throw new NoSuchEmployeeException("There is no employee with ID = " + id);
         }
         return employeeService.getEmployee(id);
+    }
+
+    @PostMapping("/employees")
+    public Employee saveEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with this id = " + id + " in Database");
+        }
+        employeeService.deleteEmployee(id);
+        return "employee with id = " + id + "was deleted";
     }
 }
